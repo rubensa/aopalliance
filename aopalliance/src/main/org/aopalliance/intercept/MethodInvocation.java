57181f9c26319d8f9e5131b1b833200409e5c322
nur <>
2003-06-30 16:37:46 +0000

    moved methods in the Invocation superclass

--- aopalliance/src/main/org/aopalliance/intercept/MethodInvocation.java
+++ aopalliance/src/main/org/aopalliance/intercept/MethodInvocation.java
@@ -3,68 +3,25 @@ package org.aopalliance.intercept;
 import java.lang.reflect.Method;
 
 /**
- * Description of an invocation to a method, given to an interceptor upon method-call.
- */
+ * Description of an invocation to a method, given to an interceptor
+ * upon method-call.
+ *
+ * <p>A method invocation is a joinpoint and can be intercepted by a method
+ * interceptor.
+ *
+ * @see MethodInterceptor */
 public interface MethodInvocation extends Invocation
 {
-    /**
-     * Invoke the next interceptor in the stack (or the target if you're last).
-     *
-     * @return the result of the call on the target (might be intercepted and changed by an interceptor).
-     * @throws Throwable if the call throws an exception.
-     */
-    Object invokeNext() throws Throwable;
-
-    /**
-     * Gets the index of the current interceptor.
-     *
-     * @return the index of the current interceptor.
-     */
-    int getCurrentInterceptorIndex();
-
-    /**
-     * Gets the total number of interceptors in the chain.
-     *
-     * @return the total number of interceptors in the chain.
-     */
-    int getInterceptorCount();
-
-    /**
-     * Gets the interceptors at the specified index.
-     *
-     * @param index index to the interceptor to retrieve.
-     * @return the interceptors at the specified index.
-     */
-    Interceptor getInterceptor(int index);
 
     /**
      * Gets the method being called.
+     *
+     * <p>This method is a frienly implementation of the {@link
+     * Joinpoint#getStaticPart()} method (same result).
      *
      * @return the method being called.
      */
     Method getMethod();
 
-    /**
-     * Gets the value of the argument at the specified index.
-     *
-     * @param index the index.
-     * @return the value of the argument.
-     */
-    Object getArgument(int index);
-
-    /**
-     * Modifies the specified argument.
-     *
-     * @param index the index of the argument.
-     * @param argument the new value of the argument.
-     */
-    void setArgument(int index, Object argument);
-
-    /**
-     * Gets the number of arguments in the call.
-     *
-     * @return the number arguments to the call.
-     */
-    int getArgumentCount();
 }
 
