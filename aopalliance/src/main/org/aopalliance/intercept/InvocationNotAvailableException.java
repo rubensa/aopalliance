fea68f559af017d4f69f073e62647eb8e56dff4c
nur <>
2003-06-30 16:41:03 +0000

    indentation

--- aopalliance/src/main/org/aopalliance/intercept/InvocationNotAvailableException.java
+++ aopalliance/src/main/org/aopalliance/intercept/InvocationNotAvailableException.java
@@ -2,11 +2,12 @@
 package org.aopalliance.intercept;
 
 /**
- * Exception thrown an attempt is made to retrieve the current Invocation
- * through an InvocationContext implementation, but no invocation information
- * is available.
- * @author Rod Johnson
- */
+ * Exception thrown when an attempt is made to retrieve the current
+ * Invocation through an InvocationContext implementation, but no
+ * invocation information is available.
+ *
+ * @author Rod Johnson */
+
 public class InvocationNotAvailableException extends AspectException {
 	
 	public InvocationNotAvailableException(String context) {
